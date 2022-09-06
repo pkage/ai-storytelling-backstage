1e3a10bacffebc4749d302dc7374cfeb8521e81d
Patrick Kage <patrick.r.kage@gmail.com>
2022-09-06 10:56:24 -0400

    redo safety filter

--- code/aist/extras/safety.py
+++ code/aist/extras/safety.py
@@ -1,10 +1,36 @@
-from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
+import numpy as np
 import torch
+import torch.nn as nn
+
+from transformers import CLIPConfig, CLIPVisionModel, PreTrainedModel
+
+# from ...utils import logging
 
 
+# logger = logging.get_logger(__name__)
 
-# safety checker
-class StableDiffusionSafetyCheckerDisable(StableDiffusionSafetyChecker):
+
+def cosine_distance(image_embeds, text_embeds):
+    normalized_image_embeds = nn.functional.normalize(image_embeds)
+    normalized_text_embeds = nn.functional.normalize(text_embeds)
+    return torch.mm(normalized_image_embeds, normalized_text_embeds.T)
+
+
+class StableDiffusionSafetyCheckerDisable(PreTrainedModel):
+    config_class = CLIPConfig
+
+    def __init__(self, config: CLIPConfig):
+        super().__init__(config)
+
+        self.vision_model = CLIPVisionModel(config.vision_config)
+        self.visual_projection = nn.Linear(config.vision_config.hidden_size, config.projection_dim, bias=False)
+
+        self.concept_embeds = nn.Parameter(torch.ones(17, config.projection_dim), requires_grad=False)
+        self.special_care_embeds = nn.Parameter(torch.ones(3, config.projection_dim), requires_grad=False)
+
+        self.register_buffer("concept_embeds_weights", torch.ones(17))
+        self.register_buffer("special_care_embeds_weights", torch.ones(3))
+
     @torch.no_grad()
     def forward(self, clip_input, images):
         pooled_output = self.vision_model(clip_input)[1]  # pooled_output
@@ -52,4 +78,3 @@ class StableDiffusionSafetyCheckerDisable(StableDiffusionSafetyChecker):
         #     )
 
         return images, has_nsfw_concepts
-
